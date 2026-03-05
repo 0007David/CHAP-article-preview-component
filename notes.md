0539b89ecc9c55dd75109e24c57fa9e354ac65f4
DaveTorrez <davidtn52@gmail.com>
2026-03-04 21:37:33 -0400

    FE: Javascript challenge - add interactivity

--- notes.md
+++ notes.md
@@ -6,10 +6,12 @@
 
 - hacer los cambios de estilos css en la forma activa
 - como posicionar la imagen correctamente
-- 
+- como cambiar color a los icon svg
+- como agregar una clase o quitar un propiedad css desde Javascript
 
 
 - Tarea pendiente
   - Maquetacion html
   - configurar style.css and javascript.js
   - Style base css (Reset, color, typhographi)
+  - Hacer para version desktop
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log('Load Script');
+
+const btnShareEl = document.getElementById('btn-share');
+const footerContent = document.getElementById('footer-content');
+const footerContentActive = document.getElementById('footer-active');
+
+const cardFooter = document.getElementById('card-footer');
+
+let showContentFooter = true;
+
+btnShareEl.addEventListener('click', () => {
+    showContentFooter = !showContentFooter;
+    console.log('Before');
+    console.log('content', footerContent);
+    console.log('content-active', footerContentActive);
+
+    if (showContentFooter) {
+        footerContent.style.display = 'flex';
+        footerContentActive.style.display = 'none'
+        cardFooter.style.background = 'none'
+        // btnShareEl.style.background = 'none';
+
+    } else {
+        footerContent.style.display = 'none';
+        footerContentActive.style.display = 'flex'
+        cardFooter.style.background = 'hsl(217, 19%, 35%)'
+        // btnShareEl.style.background = 'hsl(214, 17%, 51%)';
+
+    }
+    console.log('After');
+
+    console.log('content', footerContent);
+    console.log('content-active', footerContentActive);
+})
