99f6f6d8d2f41b7414a041f04801503386d6d924
DaveTorrez <davidtn52@gmail.com>
2026-03-11 05:48:37 -0400

    feat: add popover javascript

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const footerContent = document.getElementById('footer-content');
 const footerContentActive = document.getElementById('footer-active');
 const footerContentPopoverActive = document.getElementById('footer-active-popover');
 const cardFooter = document.getElementById('card-footer');
+const iconPath = document.getElementById('path-icon');
 let showContentFooter = true;
 
 
 btnShareEl.addEventListener('click', () => {
     showContentFooter = !showContentFooter;
     const viewportWidth = window.innerWidth;
+    console.log('icon', iconPath);
 
     if (viewportWidth > 760) {
         btnShareEl.setAttribute('popovertarget', 'tooltipLink');
@@ -20,12 +22,15 @@ btnShareEl.addEventListener('click', () => {
             footerContent.style.display = 'flex';
             footerContentActive.style.display = 'none'
             cardFooter.style.background = 'none'
-            // btnShareEl.style.background = 'none';
+            btnShareEl.style.background = 'hsl(210, 46%, 95%)';
+            iconPath.classList.replace('active', 'no-active')
+            iconPath.setAttribute('fill', 'red')
         } else {
             footerContent.style.display = 'none';
             footerContentActive.style.display = 'flex'
             cardFooter.style.background = 'hsl(217, 19%, 35%)'
-            // btnShareEl.style.background = 'hsl(214, 17%, 51%)';
+            btnShareEl.style.background = 'hsl(214, 17%, 51%)';
+            iconPath.classList.replace('no-active', 'active')
         }
     }
 })
